52adcc244a94ea672abf78c823bbe49c6e7ab42b
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-19 14:10:38 +0100

    with big hint (ndamt 3liha hhh)

--- roman-numerals-kata/src/me/elaamiri/ArabicToRomanNumeralsConvertor.java
+++ roman-numerals-kata/src/me/elaamiri/ArabicToRomanNumeralsConvertor.java
@@ -1,30 +1,95 @@
 package me.elaamiri;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ArabicToRomanNumeralsConvertor {
 	
-	private static HashMap<Integer, String> bases = 
-			(HashMap<Integer, String>) Map.of(1, "I", 5, "V", 10, "X", 50, "L", 100, "C", 500, "D", 1000, "M");
+	private static Map<Integer, String> bases = new HashMap<>();
+	public ArabicToRomanNumeralsConvertor() {
+		this.bases.put(1, "I");
+		this.bases.put(4, "IV");
+		this.bases.put(5, "V");
+		this.bases.put(9 , "IX");
+		this.bases.put(10, "X");
+		this.bases.put(40,  "XL");
+		this.bases.put(50,  "L");
+		this.bases.put(90, "XC");
+		this.bases.put(100, "C");
+		this.bases.put(400, "CD");
+		this.bases.put(500, "D");
+		this.bases.put(900, "CM");
+		this.bases.put(1000, "M");
+	}
+			
 	
-	public static String convert(int arabicNumber) {
+	public String convert(int arabicNumber) {
+		String roman = "";
+		
+		List<Integer> keysList = new ArrayList(bases.keySet());
+		Collections.sort(keysList, Collections.reverseOrder());
+		Set<Integer> keys = new LinkedHashSet(keysList);
+		
+		for(Integer key: keys) {
+			while(arabicNumber >= key) {
+				roman += bases.get(key);
+				arabicNumber -= key;
+			}
+		}
+		/*
+		if(arabicNumber == 500) return "D";
+		if(arabicNumber == 400) return "CD";
+		if(arabicNumber<500 && arabicNumber>100) return "C".repeat(arabicNumber/100);
+		if(arabicNumber == 100) return "C";
+		if(arabicNumber == 90) return "XC";
+		if(arabicNumber<100 && arabicNumber>50) return "L"+"X".repeat(arabicNumber/10 - 5);
+		if(arabicNumber == 50) return "L";
+		if(arabicNumber == 40) return "XL";
+		if(arabicNumber<50 && arabicNumber>10) return "X".repeat(arabicNumber/10);
 		if(arabicNumber == 10) return "X";
 		if(arabicNumber == 9) return "IX";
-		if(arabicNumber<9 && arabicNumber>5) return "V"+"I".repeat(arabicNumber);
+		if(arabicNumber<9 && arabicNumber>5) return "V"+"I".repeat(arabicNumber-5);
 		if(arabicNumber == 5) return "V";
 		if(arabicNumber == 4) return "IV";
-		return "I".repeat(arabicNumber);
+		*/
+		return roman;
 	}
 	
-	private static String findBase(int number) {
+	public static String findBase(int number) {
 		if(bases.containsKey(number)) return bases.get(number);
+		int temp = number / 5;
+		List<Integer> keysList = new ArrayList(bases.keySet());
+		Collections.sort(keysList, Collections.reverseOrder());
+		Set<Integer> keys = new LinkedHashSet(keysList);
 		
-		for(int key: bases.keySet()) {
-			if(number > key && number < (key*5)) return bases.get(key);
+		System.out.println(keys.toString());
+		
+		for(int key: keys) {
+			if(number >= key && Math.abs(temp - (key/5)) < 10) return bases.get(key);
 		}
 		
-		return null;
+		return "M";
+	}
+	
+	private static int findBase2(int number) {
+		if(bases.containsKey(number)) return number;
+		int temp = number / 5;
+		List<Integer> keysList = new ArrayList(bases.keySet());
+		Collections.sort(keysList, Collections.reverseOrder());
+		Set<Integer> keys = new LinkedHashSet(keysList);
+		
+		System.out.println(keys.toString());
+		
+		for(int key: keys) {
+			if(number >= key && Math.abs(temp - (key/5)) < 10) return key;
+		}
+		
+		return 1000;
 	}
 
 }
--- roman-numerals-kata/test/me/elaamiri/ArabicToRomanNumeralsConvertorTest.java
+++ roman-numerals-kata/test/me/elaamiri/ArabicToRomanNumeralsConvertorTest.java
@@ -5,43 +5,78 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ArabicToRomanNumeralsConvertorTest {
-
+	
+	ArabicToRomanNumeralsConvertor arabicToRomanNumeralsConvertor = new ArabicToRomanNumeralsConvertor();
+	
 	@Test
 	public void test1To4ArabTORoman() {
-		assertEquals("II", ArabicToRomanNumeralsConvertor.convert(2));
-		assertEquals("III", ArabicToRomanNumeralsConvertor.convert(3));
+		assertEquals("II", arabicToRomanNumeralsConvertor.convert(2));
+		assertEquals("III", arabicToRomanNumeralsConvertor.convert(3));
 
 	}
 	
 	@Test
 	public void test4ArabTORoman() {
-		assertEquals("IV", ArabicToRomanNumeralsConvertor.convert(4));
+		assertEquals("IV", arabicToRomanNumeralsConvertor.convert(4));
 
 	}
 	
 	@Test
 	public void test5ArabTORoman() {
-		assertEquals("V", ArabicToRomanNumeralsConvertor.convert(5));
+		assertEquals("V", arabicToRomanNumeralsConvertor.convert(5));
 
 	}
 	
 	@Test
 	public void test6To9ArabTORoman() {
-		assertEquals("VII", ArabicToRomanNumeralsConvertor.convert(7));
-		assertEquals("VIII", ArabicToRomanNumeralsConvertor.convert(8));
+		assertEquals("VII", arabicToRomanNumeralsConvertor.convert(7));
+		assertEquals("VIII", arabicToRomanNumeralsConvertor.convert(8));
 
 	}
 	
 	@Test
 	public void test9ArabTORoman() {
-		assertEquals("IX", ArabicToRomanNumeralsConvertor.convert(9));
+		assertEquals("IX", arabicToRomanNumeralsConvertor.convert(9));
 
 	}
 	
 	@Test
 	public void test10ArabTORoman() {
-		assertEquals("X", ArabicToRomanNumeralsConvertor.convert(10));
+		assertEquals("X", arabicToRomanNumeralsConvertor.convert(10));
 
 	}
+	
+	@Test
+	public void test10To50ArabTORoman() {
+		assertEquals("X", arabicToRomanNumeralsConvertor.convert(10));
+		assertEquals("XX", arabicToRomanNumeralsConvertor.convert(20));
+		assertEquals("XXX", arabicToRomanNumeralsConvertor.convert(30));
+		assertEquals("XL", arabicToRomanNumeralsConvertor.convert(40));
+
+
+	}
+	
+	@Test
+	public void test50To100ArabTORoman() {
+		assertEquals("LX", arabicToRomanNumeralsConvertor.convert(60));
+		assertEquals("LXX", arabicToRomanNumeralsConvertor.convert(70));
+		assertEquals("LXXX", arabicToRomanNumeralsConvertor.convert(80));
+
+	}
+	
+	@Test
+	public void test90And100ArabTORoman() {
+		assertEquals("XC", arabicToRomanNumeralsConvertor.convert(90));
+		assertEquals("C", arabicToRomanNumeralsConvertor.convert(100));
+
+	}
+	
+	@Test
+	public void test100To500ArabTORoman() {
+		assertEquals("CC", arabicToRomanNumeralsConvertor.convert(200));
+		assertEquals("CD", arabicToRomanNumeralsConvertor.convert(400));
+
+	}
+	
 
 }
