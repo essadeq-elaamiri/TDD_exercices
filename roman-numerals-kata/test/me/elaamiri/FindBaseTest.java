912c9b7a881f0a524546ff252ab5fd805b059074
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-19 17:06:26 +0100

    DONE Arabic>Roman

--- roman-numerals-kata/test/me/elaamiri/FindBaseTest.java
+++ roman-numerals-kata/test/me/elaamiri/FindBaseTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 
 public class FindBaseTest {
 
-	@Test
-	public void testFindBase() {
-		assertEquals("I", ArabicToRomanNumeralsConvertor.findBase(4));
-		assertEquals("X", ArabicToRomanNumeralsConvertor.findBase(10));
-		assertEquals("V", ArabicToRomanNumeralsConvertor.findBase(9));
-		assertEquals("X", ArabicToRomanNumeralsConvertor.findBase(29));
-		assertEquals("L", ArabicToRomanNumeralsConvertor.findBase(80));
-		assertEquals("M", ArabicToRomanNumeralsConvertor.findBase(2019));
-		assertEquals("I", ArabicToRomanNumeralsConvertor.findBase(4));
-		assertEquals("L", ArabicToRomanNumeralsConvertor.findBase(50));
-		assertEquals("C", ArabicToRomanNumeralsConvertor.findBase(133));
-		assertEquals("I", ArabicToRomanNumeralsConvertor.findBase(3));
-		assertEquals("L", ArabicToRomanNumeralsConvertor.findBase(90));
-
-	}
+	
+//	public void testFindBase() {
+//		assertEquals("I", ArabicToRomanNumeralsConvertor.findBase(4));
+//		assertEquals("X", ArabicToRomanNumeralsConvertor.findBase(10));
+//		assertEquals("V", ArabicToRomanNumeralsConvertor.findBase(9));
+//		assertEquals("X", ArabicToRomanNumeralsConvertor.findBase(29));
+//		assertEquals("L", ArabicToRomanNumeralsConvertor.findBase(80));
+//		assertEquals("M", ArabicToRomanNumeralsConvertor.findBase(2019));
+//		assertEquals("I", ArabicToRomanNumeralsConvertor.findBase(4));
+//		assertEquals("L", ArabicToRomanNumeralsConvertor.findBase(50));
+//		assertEquals("C", ArabicToRomanNumeralsConvertor.findBase(133));
+//		assertEquals("I", ArabicToRomanNumeralsConvertor.findBase(3));
+//		assertEquals("L", ArabicToRomanNumeralsConvertor.findBase(90));
+//
+//	}
 
 }
